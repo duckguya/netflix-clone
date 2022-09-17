d611b676517229b2e589747492559534406fd8c2
duckguya <duckguya@gmail.com>
2022-09-17 19:25:32 +0900

    header - add motion, Router, Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,6 @@
+function Home() {
+  return (
+    <div style={{ backgroundColor: "whiteSmoke", height: "200vh" }}>HOME</div>
+  );
+}
+export default Home;
