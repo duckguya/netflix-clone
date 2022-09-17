80aa7341288a7e869dc9fe0c9d032b65938d858b
duckguya <duckguya@gmail.com>
2022-09-18 01:19:06 +0900

    slider part one two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
@@ -34,21 +36,103 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgphoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth,
+  },
+};
+
+const offset = 6;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "now_playing"],
     getMovies
   );
-  console.log(data, isLoading);
+
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  // 슬라이드 다음 버튼을 연속으로 눌렀을 때 animation이 반복되면서 발생하는 버그를 막는다.
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totlaMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totlaMovies / offset) - 1;
+      // Math.ceil : 올림처리를 하는 함수. <-> Math.floor()
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
+
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-          <Title>{data?.results[0].title}</Title>
-          <Overview>{data?.results[0].overview}</Overview>
-        </Banner>
+        <>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            {/* 컴포넌트가 처음 마운트 될 때 animation이 실행되지 않게 한다 */}
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgphoto={makeImagePath(movie.backdrop_path, "w500")}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
       )}
     </Wrapper>
   );
