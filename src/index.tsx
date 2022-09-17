51884fd873cf6718bf63912b5656cc00ea7c4f46
duckguya <duckguya@gmail.com>
2022-09-17 21:55:52 +0900

    Home Screent part Two

--- src/index.tsx
+++ src/index.tsx
@@ -69,7 +69,8 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  color:black;
+  color:${(props) => props.theme.white.darker};
+  background-color: ${(props) => props.theme.black.veryDark};
   line-height: 1.2;
   
 }
